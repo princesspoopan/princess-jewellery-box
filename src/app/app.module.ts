2ae2450514d9808860f6e3566233ce23ceda5d4d
Krongkarn Jitsil <poopan@taskworld.com>
2017-11-08 22:18:15 +0700

    :tada::sparkles: add some routes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BuyListComponent } from './buy-list/buy-list.component';
+import { SellListComponent } from './sell-list/sell-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'buy', component: BuyListComponent },
+  { path: 'sell', component: SellListComponent },
+  { path: '', redirectTo: '/buy', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BuyListComponent,
+    SellListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sell-list/sell-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sell-list',
+  templateUrl: './sell-list.component.html',
+  styleUrls: ['./sell-list.component.css'],
+})
+export class SellListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
